33b357787c33113d7821df5d86cd446e71808de0
Francis-Bogaard <francisb1415@gmail.com>
2022-03-16 10:04:29 +0100

    nav blade update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,7 +18,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
-                        {{ __('Posts') }}
+                        {{ __('Francois posts') }}
                     </x-nav-link>
                 </div>
 
